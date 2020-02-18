d63493857381f4fd88152b58bd27b3fdb49e8695
Abdulhakeem Abdulazeez <ayoazeez26@gmail.com>
2020-02-18 15:01:06 +0100

    added functionalities

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,38 @@
 let output = document.getElementById('displayOutput');
-const buttons = document.querySelector('.buttons');
+const buttons = document.querySelectorAll('.calcButton');
+let next = false;
 
 eventListeners();
 
 function eventListeners() {
-    buttons.addEventListener('click', displayOnClick);
+    for(const button of buttons) {
+        button.addEventListener('click', displayOnClick);
+    }
+}
+
+function deleteInput() {
+    let tempArr = output.value.split("");
+    tempArr.pop();
+    output.value = tempArr.join("");
+    output.value = output.value.toString();
 }
 
 function displayOnClick(e) {
     e.preventDefault();
-    output.value += e.target.textContent;
+    if(next) {
+        clearInput();
+        next = !next;
+        output.value += e.target.textContent;
+    } else {
+        output.value += e.target.textContent;
+    }
 }
 
 function equateInputs() {
     output.value = eval(output.value);
-    console.log(output.value);
+    next = !next;
+}
+
+function clearInput() {
+    output.value = '';
 }
